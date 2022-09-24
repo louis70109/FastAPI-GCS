6a67cd65712d367969b28e9eb8e52e58acbb6eb0
louis70109 <louis70109@gmail.com>
2022-09-25 01:18:34 +0800

    ✨feat: add upload api and logging

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import os
-import uvicorn
+import tempfile
+import logging
+from utils.auth import get_current_username
+if os.getenv('API_ENV') != 'production':
+    from dotenv import load_dotenv
+    logging.info('Dev env file loading.')
+    load_dotenv()
 
-from fastapi import FastAPI, Request
+import uvicorn
+from fastapi import FastAPI, Request, Depends, File, UploadFile
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-from fastapi import File, UploadFile
 from typing import List
 
 from routers import users, items
+from utils.gcp import upload_data_to_gcs
 
 app = FastAPI()
+
+google_temp = tempfile.NamedTemporaryFile(suffix='.json')
+try:
+    GOOGLE_KEY = os.environ.get('GOOGLE_KEY', '{}')
+    google_temp.write(GOOGLE_KEY.encode())
+    google_temp.seek(0)
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = google_temp.name
+except Exception as e:
+    logging.info(f'GCP JSON key format error. {e}')
+    google_temp.close()
+    raise
+    
+
 templates = Jinja2Templates(directory="templates")
 
 app.include_router(users.router)
@@ -22,8 +42,7 @@ async def health():
 
 
 @app.get("/upload", response_class=HTMLResponse)
-async def upload_page(request: Request):
-
+async def upload_page(request: Request, username: str = Depends(get_current_username)):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/upload")
@@ -31,9 +50,13 @@ def upload(files: List[UploadFile] = File(...)):
     for file in files:
         try:
             contents = file.file.read()
-            with open(file.filename, 'wb') as f:
-                f.write(contents)
-        except Exception:
+
+            url = upload_data_to_gcs(os.getenv('GOOGLE_BUCKET'), contents, file.filename,
+                    meta=file.content_type)
+            logging.info(f'GCS upload file link: {url}')
+        except Exception as e:
+            logging.debug(e)
+            logging.warn(f'File upload to GCS fail. Please check GCP json key and bucket.')
             return {"message": "There was an error uploading the file(s)"}
         finally:
             file.file.close()
@@ -43,4 +66,7 @@ def upload(files: List[UploadFile] = File(...)):
 
 if __name__ == "__main__":
     port = os.getenv('PORT', default=5000)
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    debug = True if os.getenv('API_ENV', default='develop') == 'develop' else False
+    logging.info('FastAPI server ON!')
+    logging.info(f'Mode is {debug}.')
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=debug)
